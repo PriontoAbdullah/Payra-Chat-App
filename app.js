8d86bdd744c3e85f0c09fce5ef46e93ecbce2cf7
PriontoAbdullah <priontosoft@gmail.com>
2021-07-06 20:50:29 +0600

    User Managment & Validation

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // External Imports
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 // Internal Imports
-const { notFoundHandler, errorHandler } = require('./middlewares/common/errorHandler');
-const loginRouter = require('./router/loginRouter');
-const usersRouter = require('./router/usersRouter');
-const inboxRouter = require('./router/inboxRouter');
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
 
 // Initialize App
 const app = express();
@@ -20,18 +23,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set View Engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Parse Cookie
 app.use(cookieParser(process.env.SECRET_KEY));
 
 // Routing Setup
-app.use('/', loginRouter);
-app.use('/users', usersRouter);
-app.use('/inbox', inboxRouter);
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
 // Error Handling
 // 404 Not Found Handler
@@ -41,21 +44,20 @@ app.use(notFoundHandler);
 app.use(errorHandler);
 
 // Mongodb URL
-const mongoDB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bhwg.mongodb.net/${process
-	.env.DB_NAME}?retryWrites=true&w=majority`;
+const mongoDB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bhwg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 // Database Connection
 mongoose
-	.connect(mongoDB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(() => {
-		console.log('Database Connected');
-		app.listen(process.env.PORT, () => {
-			console.log(`Server is running on PORT ${process.env.PORT}`);
-		});
-	})
-	.catch((err) => {
-		return console.log(err);
-	});
+  .connect(mongoDB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    return console.log(err);
+  });
